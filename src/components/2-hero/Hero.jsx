c7662a2fabe926c60335b0692ffd4c0feb913581
heva505 <u2486890@rts-365.dk>
2025-05-07 14:01:27 +0200

    insta

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -28,24 +28,38 @@ const Hero = () => {
           transition={{ duration: 2 }}
           className="title"
         >
-        Every project is a new story.
+          Every project is a new story.
         </motion.h1>
 
         <p className="sub-title">
-         I'm [HEVA], a web developer specializing in creating
-          modern interactive applications and websites in front-end and back-end
-          development. <br /> I'm currently working on mastering technologies like
-          [MySQL, Node.js, and Mongo DB].  <br />I love turning creative ideas
-          into reality by writing clean and efficient code, and I'm always keen
-          to develop my skills to keep up with the latest trends in the
+          I'm [HEVA], a web developer specializing in creating modern
+          interactive applications and websites in front-end and back-end
+          development. <br /> I'm currently working on mastering technologies
+          like [MySQL, Node.js, and Mongo DB]. <br />I love turning creative
+          ideas into reality by writing clean and efficient code, and I'm always
+          keen to develop my skills to keep up with the latest trends in the
           programming world. <br />I have a great passion for solving technical
           problems and improving user experiences.
         </p>
-
         <div className="all-icons flex">
-          <div className="icon icon-instagram"></div>
-          <div className="icon icon-github"></div>
-          <div className="icon icon-linkedin"></div>
+          <a
+            href="https://www.instagram.com/heva.hev.oo0/?utm_source=ig_web_button_share_sheet"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="icon icon-instagram"
+          ></a>
+          <a
+            href="https://github.com/your_username"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="icon icon-github"
+          ></a>
+          <a
+            href="https://www.linkedin.com/in/your_username"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="icon icon-linkedin"
+          ></a>
         </div>
       </div>
 
