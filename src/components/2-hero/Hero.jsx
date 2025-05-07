9a5e944258646af1fc3c9710476fe1293d2b1c3b
heva505 <u2486890@rts-365.dk>
2025-05-07 13:25:21 +0200

    foto

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
             initial={{ transform: "scale(0)" }}
             animate={{ transform: "scale(1.1)" }}
             transition={{ damping: 6, type: "spring", stiffness: 100 }}
-            src="./images/mine-logo.jpg"
+            src="./images/im-7.png"
             className="avatar"
             alt=""
           />
